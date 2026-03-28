53fd550afefbe21f65d9a482a280787bdea2d153
blacklorddev <owinoruth525@gmail.com>
2026-03-28 08:20:03 +0300

    Update bot identity and owner details in config.js

--- config.js
+++ config.js
@@ -1,18 +1,18 @@
 module.exports = {
   // ===== Bot Identity =====
-  BOT_NAME: "𝗛𝗮𝗻𝘀𝗼𝗻 𝗫𝗠𝗗",
+  BOT_NAME: "SHADOW 𝗫𝗠𝗗",
   VERSION: "1.0.0",
 
   // ===== Core Settings =====
   PREFIX: ".",
 
   // ===== Owner =====
-  OWNER_NAME: "𝗛𝗮𝗻𝘀𝗼𝗻",
-  OWNER: ["254799959738"],
+  OWNER_NAME: "BLACKLORD",
+  OWNER: ["254739261187"],
 
   // ===== Sticker Settings =====
-  PACK_NAME: "Hanson XMD Stickers",
-  AUTHOR: "Hanson",
+  PACK_NAME: "protocol XMD Stickers",
+  AUTHOR: "BLACKLORD",
 
   // ===== Channel =====
   CHANNEL: "https://whatsapp.com/channel/0029Vb6wjlmL2ATsFBqT8c0c",
